36eab8fb8229055c07925767ef223f7af03ed465
Pedro Lopes <pedrohenrilopesribeiro@gmail.com>
2025-11-08 12:43:04 -0300

    aula do dia 08-11

--- Aula 10/frontend/js/index.js
+++ Aula 10/frontend/js/index.js
@@ -8,20 +8,35 @@ const carregar = () => {
                 const td_desc = document.createElement('td');
                 const td_saldo = document.createElement('td');
                 const td_preco = document.createElement('td');
+                const td_acoes = document.createElement('td');
                 //adicionando os tds nos trs
                 tr.appendChild(td_id);
                 tr.appendChild(td_desc);
                 tr.appendChild(td_saldo);
                 tr.appendChild(td_preco);
+                tr.appendChild(td_acoes);
                 //adicionando ao tbbody
                 tbbody.appendChild(tr);
                 //colocando os valores nas tds
                 td_id.textContent = item.id;
                 td_desc.textContent = item.descricao;
-                td_saldo.textContent = item.td_saldo;
-                td_preco.textContent = item.td_preco
+                td_saldo.textContent = item.saldo;
+                td_preco.textContent = item.preco
+                td_acoes.innerHTML=`<a href="cadastro.html?id=${item.id}">
+                <img src="images/edit.svg" class="img-edit"></a> | 
+                <img src="images/delete.svg" class="img-delete" onclick="apagar(${item.id});">`
             })
         })
+}
+
+const apagar = async (id) => {
+    const op = confirm("Deseja mesmo excluir o registro " + id + " ?");
+     if (op) {
+        await axios.delete("http://localhost:3000/produtos/"+id);
+        window.location.reload();
+     } else {
+        console.log("cancelei a deleção!");
+     }
 }
 
 carregar(); 
